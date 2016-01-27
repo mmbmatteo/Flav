998f7f9809ad1deb6a29a8e3254cfc61d09a9e68
mmbmatteo <matteom.bianchini@gmail.com>
2016-01-27 15:03:42 +0530

    Enanched code, display posts

---
+++ PostDB.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class PostDB {
+	static ArrayList<Post> posts = Demo.getPosts();
+	static HashMap<String, ArrayList> allCatPosts = new HashMap();
+	
+	static void init() {
+		for (Post p : posts) {
+			if (!allCatPosts.containsKey(p.getCategory())) {
+				ArrayList<Post> catList = new ArrayList<Post>(); 
+				allCatPosts.put(p.getCategory(), catList);
+			}
+			allCatPosts.get(p.getCategory()).add(p);
+		}
+	}
+	
+	// Access methods
+    public static List<Post> getPosts() {
+		return posts; 	
+	}
+
+    public static List<Post> getPosts(String category) {
+		return allCatPosts.get(category);
+    }
+	
+	public static Post answerPost(Post post, String answerBody) {
+		Post answer = post.clone();
+		answer.setDescription(answerBody);
+		answer.setType("answer");
+	return answer;
+    }
+	
+	public static void addPost(Post post){
+		posts.add(post);
+		allCatPosts.get(post.getCategory()).add(post);
+	}
+	
+	public static HashMap getPostsHashMap(){
+		return allCatPosts;
+	}
+	
+	public static void printCategories(){
+	System.out.println(Arrays.toString(PostDB.getPostsHashMap().keySet().toArray()));
+	}
+
+//    abstract Post     updatePost(Post post);
+//    abstract boolean  deletePost(Post post);
+//    abstract User     flagPost(Post post);
+}
